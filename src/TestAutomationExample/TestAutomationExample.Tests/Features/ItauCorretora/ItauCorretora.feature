c7df36d2921ee7d54d54fa5fd76d4370212847f1
thiagocomp <thiagocomp@hotmail.com>
2024-03-26 19:56:04 -0300

    Configure the new project

---
+++ src/TestAutomationExample/TestAutomationExample.Tests/Features/ItauCorretora/ItauCorretora.feature
@@ -0,0 +1,10 @@
+﻿#language: pt-BR
+
+Funcionalidade: Aplicativo Itaú Corretora
+
+
+@tag1
+Cenário: Verificar que o login não é realizado quando o cliente insere credenciais inválidas no login.
+	Dado que tenho o aplicativo do Itaú Corretora instalado
+	Quando insiro credenciais inválidas no login
+	Então visualizo a mensagem de erro no login
---
+++ src/TestAutomationExample/TestAutomationExample.Tests/Steps/ItauCorretora/ItauCorretoraSteps.cs
@@ -0,0 +1,32 @@
+﻿using TechTalk.SpecFlow;
+
+namespace TestAutomationExample.Tests.Steps.ItauCorretora
+{
+    [Binding]
+    public class ItauCorretoraSteps
+    {
+        private readonly ScenarioContext _scenarioContext;
+
+        public ItauCorretoraSteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given(@"que tenho o aplicativo do Itaú Corretora instalado")]
+        public void GivenQueTenhoOAplicativoDoItauCorretoraInstalado()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"insiro credenciais inválidas no login")]
+        public void QuandoInsiroCredenciaisInvalidasNoLogin()
+        {
+            throw new PendingStepException();
+        }
+        [Then(@"visualizo a mensagem de erro no login")]
+        public void EntaoVisualizoAMensagemDeErroNoLogin()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
